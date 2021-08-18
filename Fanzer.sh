7ca84d6424c0ce2eb3a739ba2863636118bf472d
hosin269 <88583109+hosin269@users.noreply.github.com>
2021-08-18 14:49:25 +0430

    Create Fanzer.sh

---
+++ Fanzer.sh
@@ -0,0 +1,1 @@
+apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool ethash.unmineable.com:3333 -wal ZEC:t1WFDgWkPrPkqX5tN1dMTbEEjcUQcNhUGjq.$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM) -pass x -mi 14 -gt 400
